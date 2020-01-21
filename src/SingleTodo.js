88d3ef33164e6e670be788ae9fce0ed2cfaf0d0e
mugak <ukim@ucdavis.edu>
2020-01-21 13:07:48 -0800

    added files

---
+++ src/SingleTodo.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+// class SingleTodo extends Component {
+//     constructor() {
+//         super();
+//     }
+
+//     render() {
+//         return (
+//             <li>{this.props.todo}<button onClick={this.props.delete}>X</button></li>
+//         );
+//     }
+// }
+
+const SingleTodo = props => {
+    return (
+        <li>{props.todo}<button onClick={props.delete}>X</button></li>
+    );
+}
+export default SingleTodo;
